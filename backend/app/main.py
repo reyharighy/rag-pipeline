1149b7f33c1bed39b749c7e8263f72236a2dc73d
Muhammad Reyhan Arighy <arighymoch@gmail.com>
2026-05-01 20:42:05 +0700

    Feat file upload (#1)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.routes import upload
 
 app = FastAPI()
 
-@app.get("/")
+app.include_router(upload.router)
+
+@app.get("/health")
 def read_root():
     return {"message": "Hello from backend!"}
