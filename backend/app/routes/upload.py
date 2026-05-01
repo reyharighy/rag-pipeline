23dc6f822c90c89a77a2343e2357eee67ba8596d
Muhammad Reyhan Arighy <arighymoch@gmail.com>
2026-05-01 22:35:55 +0700

    refactor file compatibility check

--- backend/app/routes/upload.py
+++ backend/app/routes/upload.py
@@ -8,26 +8,28 @@ router = APIRouter()
 
 ALLOWED_TYPES = ["application/pdf", "text/plain"]
 
+def check_file_compatibility(file: UploadFile):
+    if file.content_type not in ALLOWED_TYPES:
+        return {
+            "status_code": 400,
+            "detail": "Only PDF and TXT allowed",
+        }
+
+    return True
+
 @router.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
-    if file.content_type not in ALLOWED_TYPES:
-        raise HTTPException(
-            status_code=400,
-            detail="Only PDF and TXT allowed",
-        )
+    file_compatibility = check_file_compatibility(file)
 
-    if file.filename is None:
-        raise HTTPException(
-            status_code=400,
-            detail="File must have name",
-        )
+    if file_compatibility is True:
+        file_path = STORAGE_DIR / Path(file.filename or "no-filename")
 
-    file_path = STORAGE_DIR / Path(file.filename)
+        with file_path.open("wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
 
-    with file_path.open("wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+        return {
+            "filename": file.filename,
+            "message": "File uploaded successfully"
+        }
 
-    return {
-        "filename": file.filename,
-        "message": "File uploaded successfully"
-    }
+    raise HTTPException(**file_compatibility)
