05d51c158dcb0eae007e171ee74060a2b11e5e8c
Muhammad Reyhan Arighy <arighymoch@gmail.com>
2026-05-02 13:28:53 +0700

    enquque job inside /upload endpoint and provide another to get job status

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,10 @@
 from .storage import STORAGE_DIR
+from .services import pipeline_queue, redis_conn
+from .workers import process_file
 
 __all__ = [
-    "STORAGE_DIR"
+    "STORAGE_DIR",
+    "pipeline_queue",
+    "redis_conn",
+    "process_file",
 ]
