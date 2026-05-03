7dff133389d5dcc60c8592daff2a24f54eca34be
Muhammad Reyhan Arighy <arighymoch@gmail.com>
2026-05-04 00:47:27 +0700

    Feat chat agent (#21)
    * early stege of agent module
    
    * add chat agent endpoint using SSE

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -1,7 +1,9 @@
+from .chat import router as chat_endpoint
 from .health import router as health_endpoint
 from .upload import router as upload_endpoint
 
 __all__ = [
+    "chat_endpoint",
     "health_endpoint",
     "upload_endpoint",
 ]
