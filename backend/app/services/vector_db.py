bb356293f0c613eec1a2ba54c0d821627e201263
Muhammad Reyhan Arighy <arighymoch@gmail.com>
2026-05-04 20:54:42 +0700

    use embeddings from cohere

--- backend/app/services/vector_db.py
+++ backend/app/services/vector_db.py
@@ -5,13 +5,20 @@ from sqlalchemy.exc import ProgrammingError
 
 from app.services import get_embedding_service
 
-VECTORDB_URL = os.getenv("VECTORDB_URL", "")
-VECTOR_SIZE = int(os.getenv("VERTOR_SIZE", 768))
+VECTORDB_URL = os.getenv("VECTORDB_URL", None)
+
+if VECTORDB_URL is None:
+    print("'VECTORDB_URL' is not found")
+
+VECTOR_SIZE = os.getenv("VECTOR_SIZE", 512)
+
+if VECTOR_SIZE is 0:
+    print("'VECTOR_SIZE' is not found")
 
 
 @lru_cache(maxsize=1)
 def get_vector_db_engine():
-    return PGEngine.from_connection_string(VECTORDB_URL)
+    return PGEngine.from_connection_string(str(VECTORDB_URL))
 
 
 def get_vector_db_service():
@@ -26,7 +33,7 @@ def init_vector_db():
     try:
         get_vector_db_engine().init_vectorstore_table(
             table_name="documents",
-            vector_size=VECTOR_SIZE,
+            vector_size=int(VECTOR_SIZE),
         )
     except ProgrammingError:
         pass
