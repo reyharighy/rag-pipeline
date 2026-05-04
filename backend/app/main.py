76617747a6fe64afaa803d3ab1e629a97dbbd2d7
Muhammad Reyhan Arighy <arighymoch@gmail.com>
2026-05-04 20:56:15 +0700

    Feat rag pipeline (#25)
    * no need for app scope module
    
    * use import from dedicated module
    
    * take out embedding process
    
    embed and store is defined from langchain integration of PGVectorStore
    
    * outsource docs_transformer
    
    * the job should be renamed into file_embedder (more specific)
    
    * rename the queue for file embed only
    
    by this we can extend the job types in the future
    
    * integrate file embedding job into upload endpoint
    
    * the function name should be more specific
    
    * use embeddings from cohere

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,13 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
-from app.agent import Graph
 from app.api import routers
 from app.services import init_vector_db
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(_: FastAPI):
     init_vector_db()
-    app.state.graph = Graph().build_graph()
-
     yield
 
 
