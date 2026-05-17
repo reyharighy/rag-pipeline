65a9337183ccd37f1fefc3c3902bfe3b6d2a5ab3
Muhammad Reyhan Arighy <arighymoch@gmail.com>
2026-05-17 23:10:42 +0700

    abstraction for vector docs

--- backend/app/database/tables/vector_document.py
+++ backend/app/database/tables/vector_document.py
@@ -1,5 +1,9 @@
 import logging
+from typing import Any, Optional
 
+from langchain_core.documents import Document
+from langchain_core.embeddings import Embeddings
+from langchain_postgres import PGVectorStore
 from sqlalchemy.exc import ProgrammingError
 
 from .base import Table
@@ -12,8 +16,12 @@ _embedding_cfg = get_settings().embedding
 VECTOR_STORE_TABLE_NAME = "documents"
 
 class VectorDocument(Table):
-    def __init__(self, id: str):
-        self.id = id
+    def __init__(self, embedding_service: Embeddings):
+        self.service = PGVectorStore.create_sync(
+            engine=_database_cfg.engine,
+            table_name=VECTOR_STORE_TABLE_NAME,
+            embedding_service=embedding_service,
+        )
 
     @staticmethod
     def create_table() -> None:
@@ -45,3 +53,43 @@ class VectorDocument(Table):
                 "Vector store table '%s' is ready.",
                 VECTOR_STORE_TABLE_NAME,
             )
+
+    def add(self, **kwargs: Any) -> None:
+        entry = kwargs.get("entry", None)
+
+        if entry is None:
+            raise ValueError("entry is required")
+
+        if isinstance(entry, list) and not all(
+            isinstance(r, Document) for r in entry
+        ):
+            raise ValueError("entry is a list that contains non-BaseMessage items")
+
+        if not isinstance(entry, list) and not isinstance(entry, Document):
+            raise ValueError("entry is not a list or Document")
+
+        if isinstance(entry, Document):
+            entry = [entry]
+
+        self.service.add_documents(entry)
+
+    def get(self, **kwargs: Any) -> Any:
+        query = kwargs.get("query", None)
+
+        if query is not None:
+            raise NotImplementedError("query is not implemented")
+
+        raise NotImplementedError("VectorDocument does not support getting documents")
+
+    def similarity_search(self, **kwargs: Any) -> Any:
+        query = kwargs.get("query", None)
+
+        if query is None:
+            raise ValueError("query is required")
+
+        top_k = kwargs.get("top_k", 8)
+
+        if not isinstance(top_k, int):
+            raise ValueError("top_k must be an integer")
+
+        return self.service.similarity_search(query, top_k)
